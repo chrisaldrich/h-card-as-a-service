c7491dca1a2ca6f65a0cd0d483a9c2005bd5f096
Shane Becker <veganstraightedge@gmail.com>
2016-06-27 16:08:55 -0700

    Ensure http:// or https:// at the start of personal_website

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,8 +5,18 @@ class Person < ActiveRecord::Base
   
   before_update :generate_domain
   before_create :generate_domain
+  before_update :ensure_protocol
+  before_create :ensure_protocol
   
+  PROTOCOL_REGEX = /^https*:\/\//
+
   def generate_domain
-    self.domain = self.personal_website.gsub(/https*:\/\//, "")
+    self.domain = self.personal_website.gsub(PROTOCOL_REGEX, "")
+  end
+
+  def ensure_protocol
+    unless self.personal_website =~ PROTOCOL_REGEX
+      self.personal_website = "http://#{self.personal_website}"
+    end
   end
 end
