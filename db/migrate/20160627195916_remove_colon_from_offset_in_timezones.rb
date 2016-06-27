e1514493ad29eebdaeee0ce14835249ffbecbb2d
Shane Becker <veganstraightedge@gmail.com>
2016-06-27 13:03:41 -0700

    Remove colon from time offsets

---
+++ db/migrate/20160627195916_remove_colon_from_offset_in_timezones.rb
@@ -0,0 +1,14 @@
+class RemoveColonFromOffsetInTimezones < ActiveRecord::Migration
+  def up
+    Timezone.all.each do |timezone|
+      timezone.offset = timezone.offset.sub(":", "")
+      timezone.save!
+    end
+  end
+  def down
+    Timezone.all.each do |timezone|
+      timezone.offset = timezone.offset.sub(/(\d{2})$/, ":$1")
+      timezone.save!
+    end
+  end
+end
